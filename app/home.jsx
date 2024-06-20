b64ae6b5bcf454246ebb1ff7660104aab0c686ee
Gbemiga Salam <holuwagbemiga97@gmail.com>
2024-06-20 15:11:10 +0100

    imageslider

--- app/home.jsx
+++ app/home.jsx
@@ -1,11 +1,69 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import tw from 'twrnc'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import ImageSlider from '../components/ImageSlider';
+
 
 const Home = () => {
   return (
-    <View>
-      <Text>This home page</Text>
-    </View>
+    <SafeAreaView style={tw`flex-1 bg-white flex space-y-5 pt-4`} edges={['top']}>
+
+      {/* punchline and avatar */}
+      <View style={tw`flex-row justify-between items-center mx-5`}>
+        <View className="space-y-2">
+          <Text
+          style={{
+            fontSize: hp(4.5),
+            fontWeight: 'bold',
+            letterSpacing: "0.05em",
+            textTransform: 'uppercase'
+          }}
+          className='text-neutral-700'
+          >
+            Ready To
+          </Text>
+          <Text
+          style={{
+            fontSize: hp(4.5),
+            fontWeight: 'bold',
+            letterSpacing: "0.05em",
+            textTransform: 'uppercase'
+          }}
+          className='text-rose-500'
+          >
+            Workout
+          </Text>
+        </View>
+
+        <View style={tw`flex justify-center items-center gap-y-2`}>
+          <Image 
+          source={require('../assets/images/avatar.webp')}
+          style={{height: hp(6), width: hp(6)}}
+          className='rounded-full'
+          />
+          <View 
+          style={{
+            height: hp(5.5),
+            width: hp(5.5),
+            borderRadius: '999px',
+            display: "flex",
+            justifyContent: 'center',
+            alignItems: "center",
+            border: "3px solid lightgray"
+          }}
+          className='bg-neutral-200'>
+            <Ionicons name='notifications' size={hp(3)} color="gray" />
+          </View>
+        </View>
+      </View>
+
+      {/* image slider */}
+      <View>
+        <ImageSlider />
+      </View>
+    </SafeAreaView>
   )
 }
 
---
+++ constants/index.js
@@ -0,0 +1,9 @@
+
+
+export const sliderImages = [
+    require('../assets/images/gym1.webp'),
+    require('../assets/images/gym2.webp'),
+    require('../assets/images/gym3.webp'),
+    require('../assets/images/gym4.webp'),
+    require('../assets/images/gym5.webp'),
+]
